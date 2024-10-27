995990a55bd4aa4be8d987d8bb2538fdb56d32b9
Firsova Polina <firpoly@gmail.com>
2024-10-27 22:50:19 +0300

    Домашняя работа

--- src/main/java/com/example/RestTask/controller/AuthorizationController.java
+++ src/main/java/com/example/RestTask/controller/AuthorizationController.java
@@ -5,10 +5,17 @@ import com.example.RestTask.model.Authorities;
 import com.example.RestTask.model.User;
 import com.example.RestTask.service.AuthorizationService;
 import jakarta.validation.Valid;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+import java.io.IOException;
 import java.util.List;
 
 
@@ -21,5 +28,13 @@ public class AuthorizationController {
     public List<Authorities> getAuthorities(@Valid User user) {
         service = new AuthorizationService();
         return service.getAuthorities(user);
+    }
+    @GetMapping("/signin")
+    public ResponseEntity<Resource> getForm() throws IOException {
+        Resource resource = new ClassPathResource("templates/Sign.html");
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(resource);
+
     }
 }
